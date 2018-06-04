2c6357c0d104533b96ef41d6c30e6098660d34e9
Mark Myers <markalope@gmail.com>
2018-06-04 13:51:10 -0700

    created search file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
+  <div id="app">
+      <SearchControl :onSearch="handleSearch" />
+  </div>
   
 </template>
 
 <script>
-export default {
+import SearchControl from './components/SearchControl';
 
-}
+// eslint-disable-next-line
+console.log(process.env.VUE_APP_API_KEY);
+
+export default {
+  data() {
+    return {
+      people: null
+    };
+  },
+  components: {
+    PeopleList,
+    SearchControl
+  },
+  methods: {
+    handleSearch(name) {
+      getPeople(name).then(data => {
+        this.people = data.results;
+      });
+    }
+  }
+};
 </script>
 
 <style>
