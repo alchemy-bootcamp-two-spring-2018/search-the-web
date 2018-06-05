d67f5d8bcd026ca7154d4f064b5594596e5d559d
rbtprograms <rbt61690@gmail.com>
2018-06-04 17:11:07 -0700

    eslintrc and some changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
-export function getNews(name) {
-    //eslint-disable-next-line
-    const apiKey = process.env.VUE_APP_API_KEY;
-    const url = 'https://newsapi.org/v2/everything?' +
+export default function getNews(name) {
+  //eslint-disable-next-line
+  const apiKey = process.env.VUE_APP_API_KEY;
+  const url = 'https://newsapi.org/v2/everything?' +
     'q=' + encodeURIComponent(name) +
     '&sortBy=publishedAt&' +
-    'apiKey=' + apiKey
+    'apiKey=' + apiKey;
 
     
     
-    return fetch(url)
-        .then(response => response.json());
+  return fetch(url)
+    .then(response => response.json());
 }
