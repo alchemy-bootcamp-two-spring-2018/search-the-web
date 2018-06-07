4d7091292badab87d6e43e2d7877a32e8bc7cacf
Antreo Pukay <acrosswalker@yahoo.com>
2018-06-07 14:13:40 -0700

    actually completed

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,10 @@
 // eslint-disable-next-line
-export function getNews() {
+export function getNews(topic) {
   const apiKey = process.env.VUE_APP_API_KEY;
-  console.log('api key', apiKey)
+  console.log('api key', apiKey);
   const url = 'https://newsapi.org/v2/everything?' +
-              'q=Apple&' +
-              'from=2018-06-04&' +
-              'sortBy=popularity&' +
-              'apiKey=784b560036d447e8b3bdbe91b17078e8';
+              'q=' + encodeURIComponent(topic) +
+              '&apiKey=' + apiKey;
   return fetch(url)
     .then(response => response.json());
 }
