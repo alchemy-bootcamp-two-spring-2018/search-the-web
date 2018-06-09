30e213412ec10635cc3243f9d3f2bca5b11e61d5
Rehmsy <sarahrehmer@gmail.com>
2018-06-09 16:40:07 -0700

    fixed all problems, except getting api key variable to work in place of actual api key in api.js

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Uses `fetch` and VueJS to read data from a third-party news API, and allows user to search any news topic.  
 
+Get your own News Api key here:
 https://newsapi.org
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,20 @@
 export function getNews(topic) {
-  const url = 'https://newsapi.org/v2/everything?q=' +
-  encodeURIComponent(topic) + '&apiKey=3e0ba7546dd843edb2af015fcbad1fe4';
+  
+  // eslint-disable-next-line
+  const apiKey = process.env.VUE_APP_API_KEY;
+  console.log('api key ' + apiKey);
+  //why is apiKey coming back undefined? seems to not be accessing the defined variable in .env file.
+  //what did I do wrong?
+
+  const url = 'https://newsapi.org/v2/everything?q=' + 
+  encodeURIComponent(topic) + 
+  
+  // because 'undefined' the following does not work: 
+  // '&apiKey=' + apiKey;
+  
+  //keeping hard-coded key below until figured out.
+  '&apiKey=3e0ba7546dd843edb2af015fcbad1fe4';
+
   console.log('url', url);
   return fetch(url)
     .then(response => response.json());
