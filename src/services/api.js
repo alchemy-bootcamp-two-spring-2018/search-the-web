a7d1461943f299f2af8225a6db4ddc53ef04cb17
Mark Myers <markalope@gmail.com>
2018-06-14 14:09:24 -0700

    replaced API key in api.js with link to .env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
+
+const apiKey = process.env.VUE_APP_API_KEY;
+  
 export function getArticles(title) {
 
-  const url = 'https://newsapi.org/v2/top-headlines?country=us&q=' + title + '&apiKey=135cc32856a14a309805e4b35cc4af0a';
+  const url = 'https://newsapi.org/v2/top-headlines?country=us&q=' + title + '&apiKey=' + apiKey;
   var req = new Request(url);
   return fetch(req)
     .then(response => response.json()); 
